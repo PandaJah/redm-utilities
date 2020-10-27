d5225114f74fac0ec9fe34abcaa6c0755de0ac76
Ryan Lockard <ryan.jesse.lockard@gmail.com>
2020-10-26 21:53:48 -0400

    impl utilities features

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,1 +1,16 @@
-import { Delay } from "../util"
+import chalk from "chalk"
+import { logLevelFromStr } from "../util"
+import * as Config from "../../config.json"
+
+on("utilities:logServer", (event: LogEvent) => {
+  const
+    logLevel = logLevelFromStr(event.level),
+    configLevel = logLevelFromStr(Config.utilities.logLevel.server)
+
+  if (configLevel <= logLevel) {
+    console.log(chalk.redBright(
+      chalk.blueBright(chalk.bold(`[${event.title || "Client"}]:`)),
+      event.output
+    ))
+  }
+})
