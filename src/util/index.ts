14fd8e590234f0d21fafa9c2fc6efdbf321d5206
Ryan Lockard <ryan.jesse.lockard@gmail.com>
2020-10-26 21:44:50 -0400

    added logLevelFromStr util func to parse string-levels into ints

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,3 +1,24 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 
 export const Delay = (ms: number) => new Promise(res => setTimeout(res, ms));
+
+export const logLevelFromStr = (log: string): number => {
+  const logCased = log.toLowerCase()
+  switch (logCased) {
+    case "debug": {
+      return 1
+    }
+
+    case "info": {
+      return 2
+    }
+
+    case "warn": {
+      return 3
+    }
+
+    case "error": {
+      return 4
+    }
+  }
+}
