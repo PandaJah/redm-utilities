bceb61f1475a134aa27748aa28a8e44de304fdd9
Ryan Lockard <ryan.jesse.lockard@gmail.com>
2020-10-27 21:56:41 -0400

    made the relation between config "key" and result more aparent

--- src/client/client.ts
+++ src/client/client.ts
@@ -6,7 +6,7 @@ on("onClientResourceStart", () => {
   // Show or continue hiding FoW
   SetMinimapHideFow(Config.utilities.clearMap)
   // Show or Hide the player's HUD
-  DisplayHud(Config.utilities.hideHud)
+  DisplayHud(!Config.utilities.hideHud)
 })
 
 on("utilities:logClient", (event: LogEvent) => {
